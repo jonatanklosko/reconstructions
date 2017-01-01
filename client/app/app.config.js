19fbdea2c222ba7492b4670a5a6681682c493971
Jonatan Kłosko <jonatanklosko@gmail.com>
2017-01-01 22:23:00 +0100

    Updates

--- client/app/app.config.js
+++
@@ -1,6 +0,0 @@
-export default ($locationProvider, $urlRouterProvider) => {
-  'ngInject';
-
-  /* Set the default route. */
-  $urlRouterProvider.otherwise('/');
-};
--- client/app/views/layout/index.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-
-import LayoutController from './layout.controller';
-
-export default angular
-  .module('app.view.layout', [])
-  .controller('LayoutController', LayoutController)
-  .name;
