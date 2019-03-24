985848533ad20fa9de5c2c0cf60aa49d47083e42
Jonatan Kłosko <jonatanklosko@gmail.com>
2019-03-24 22:08:48 +0100

    Improve moves formatting.

--- src/logic/moves.js
+++ src/logic/moves.js
@@ -14,8 +14,14 @@ const PATTERN_BY_MOVE_TYPE = {
 
 const MOVES_REGEXP = /([RLUDFB]w?|[rludfbMSExyz])2?'?/g;
 
-export const stringToMoves = string =>
-  (string && string.replace(/\s+/g, '').match(MOVES_REGEXP)) || [];
+export const stringToMoves = string => {
+  if (!string) return [];
+  const preformatted = string
+    .replace(/\s+/g, '')
+    .replace(/`/g, "'")
+    .replace(/[XYZ]/g, rotation => rotation.toLowerCase());
+  return preformatted.match(MOVES_REGEXP) || [];
+};
 
 export const shrink = string =>
   stringToMoves(string).join('');
